109e7e6566feebb37db8016ffee0af4abda9c0df
Kudrot467 <kkudrotekhoda@gmail.com>
2023-10-20 00:10:44 +0600

    brand details added

--- src/Components/BrandDetails/BrandDetails.jsx
+++ src/Components/BrandDetails/BrandDetails.jsx
@@ -1,15 +1,40 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BrandDetails = () => {
-    const {brandName}=useParams();
-    
-    console.log(brandName);
+  const allProducts = useLoaderData();
+  console.log(allProducts);
+  const { brand_name } = useParams();
+  const products = allProducts.filter(
+    (products) => products.brand_name === brand_name
+  );
 
-    return (
-        <div>
-            
+  console.log(brand_name);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      {products.map((product) => (
+        <div key={product.id} >
+          <div className="card card-compact bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src={product.image_url}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{product.name}</h2>
+              <p>{product.short_description}</p>
+              <p>{product.price}</p>
+              <div className="card-actions justify-end">
+                <button className="btn">Details</button>
+                <button className="btn btn-primary">Update</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default BrandDetails;
